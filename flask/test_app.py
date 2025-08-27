5c06e8e03f9b9c14ea153d3665134ea8f48b978f
Ayyappan1502 <srayyappan05@gmail.com>
2025-08-28 04:25:11 +0530

    commit all code with yml file

--- flask/test_app.py
+++ flask/test_app.py
@@ -2,6 +2,6 @@ from app import app
 
 # Test the home route
 def test_home():
-    response = app.test_client().get('/')
+    response = app.test_client().get('/about')
     assert response.status_code == 200 
     assert response.data == b"Welcome to the Home Page!"
